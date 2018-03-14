dadf61b05fefae483c076569b47ae09bf0272aeb
OdineiRibeiro <odinei.ribeiro@live.com>
2018-03-14 20:50:09 -0300

    lesson 3

---
+++ 03_template_with_jinja2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    names_of_instructors = ['Elie', 'Tim', 'Matt']
+    random_name = 'Tom'
+    return render_template('index.html', names=names_of_instructors, name=random_name)
+
+@app.route('/second')
+def second():
+    return 'WELCOME TO THE SECOND PAGE'
+
+@app.route('/title')
+def title():
+    return render_template('title.html')
