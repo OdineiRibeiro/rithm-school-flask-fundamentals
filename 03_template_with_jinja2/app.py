d61fe8f56494a80f7762481f90a04a9fc90e302c
OdineiRibeiro <odinei.ribeiro@live.com>
2018-03-16 05:53:13 -0300

    jinja templates

--- 03_template_with_jinja2/app.py
+++ 03_template_with_jinja2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,3 +15,30 @@ def second():
 @app.route('/title')
 def title():
     return render_template('title.html')
+
+@app.route('/show-form')
+def show_form():
+    return render_template('first-form.html')
+
+@app.route('/data')
+def print_name():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return f"You put {first} {last}"
+
+@app.route('/person/<name>/<int:age>')
+def person(name, age):
+    return render_template('person.html', name=name, age=age)
+
+@app.route('/calculate')
+def calculate():
+    return render_template('calculate.html')
+
+@app.route('/math')
+def fname():
+    import pdb; pdb.set_trace()
+
+    operation = request.args.get('operation')
+    number1 = request.args.get('number1')
+    number2 = request.args.get('number2')
+    return render_template("result.html")
